3e6c7a254cae862f5ca2f883bf34f606daf05dac
Bithi-84 <tehzeebbithi.2016@gmail.com>
2024-11-09 19:29:56 +0600

    JS LOOP

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// console.log("Hello world")
+// console.log("Hello world")
+// console.log("Hello world")
+// console.log("Hello world")
+// console.log("Hello world")
+// console.log("Hello world")
+
+// for(let i=1; i<=10; i++){
+//     console.log("Hello world");
+//     console.log(i);
+// }
+
+let marks = [23, 67, 89, 99, 33];
+
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
+
+// for(let i=0; i<=4; i++){
+//     console.log(marks[i]);
+// }
+
+let numbers = [34, 56, 67, 90, 45, 74, 98, 123,76, 100];
+
+for(let i=0; i<=9; i++){
+   
+    if(numbers[i] %2 == 0){
+        console.log("Even Number="+numbers[i]);
+    }
+    else{
+        console.log("Odd Number="+numbers[i]);
+    } 
+}
+
